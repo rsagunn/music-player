1f736ee051e0adabbdbd3a51eef1d9832c35c7af
rsagunn <reilans737@deltalearns.ca>
2026-02-03 13:22:28 -0800

    psuedocode

---
+++ main.py
@@ -0,0 +1,30 @@
+#import
+
+
+
+#open mp3 folder
+
+
+
+
+#scan for mp3 files and metadata
+
+
+
+
+#display mp3 files in a list with metadata
+
+
+
+
+#select songs and show all controls
+
+
+
+
+
+#keep playing songs in a loop until user exits
+
+
+
+
