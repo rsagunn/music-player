26d4019d1e8380997ccc72ff60fa658713f49796
rsagunn <reilans737@deltalearns.ca>
2026-02-05 11:15:12 -0800

    Added a button to load folders
    Defined load folder so tk.button can run that func. I also made it to load mp3 metadata but if didnt then it resorts to my premade titles.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-playlist = []
-current_song = 0
+playlist = [] # list of songs
+current_song = 0 # info of current song
+paused = False # paused
 
 import os
 import tkinter as tk
@@ -11,17 +12,34 @@ from mutagen.easyid3 import EasyID3
 pygame.init()
 pygame.mixer.init()
 
-#window
-window = tk.Tk()          # create the window
-window.title("Nightwave") # window title
-window.geometry("400x300")# size (width x height)
-window.configure(bg="#363636") # background color
-
-window.mainloop()    
-
 os.listdir
 
 #scan for mp3 files and metadata
+
+def load_folder(): #func load folder
+    global playlist, current_song # global vars
+
+    folder = filedialog.askdirectory() # opens folder selector
+    if folder == "": # if no folder selected
+        return # go back
+    
+
+    playlist = [] 
+    current_song = 0
+
+    for file in os.listdir(folder): # scan files in folder
+        if file.lower().endswith(".mp3"): # lower case and check if end w/ .mp3
+            path = os.path.join(folder, file) # full path
+
+            try:
+                audio = MP3(path, ID3=EasyID3) # get mp3 metadata
+                title = audio.get("title", [file])[0] # title
+                artist = audio.get("artist", ["Unknown Artist"])[0] # artist
+            except:
+                title = file # if no metadata included
+                artist = "Unknown Artist" # use this name
+
+
 
 
 
@@ -44,5 +62,14 @@ os.listdir
 
 #add a fullscreen feature
 
+# window
+window = tk.Tk() # create the window
+window.title("Nightwave") # window title
+window.geometry("400x300") # size
+window.configure(bg="#363636") # background color
 
 
+tk.Button(window, text="Load Folder", command=load_folder).pack(pady=5)   
+
+
+window.mainloop() 
