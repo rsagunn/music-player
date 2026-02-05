db45e348f221a7d2d2d8cd6f6e808d84f6fedd4b
rsagunn <reilans737@deltalearns.ca>
2026-02-05 10:32:16 -0800

    Added imports and GUI window
    Imported os, mutagen, tkinter, and time. When main.py is ran it creates a window with a dark theme

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-#import
+playlist = []
+current_song = 0
 
+import os
+import tkinter as tk
+from tkinter import filedialog
+import time
+import pygame
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+pygame.init()
+pygame.mixer.init()
 
+#window
+window = tk.Tk()          # create the window
+window.title("Nightwave") # window title
+window.geometry("400x300")# size (width x height)
+window.configure(bg="#363636") # background color
 
-#open mp3 folder
+window.mainloop()    
 
-
-
+os.listdir
 
 #scan for mp3 files and metadata
 
